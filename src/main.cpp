15b906c450c7b0e8fe8980883fa1feffa8716efd
Mauro Gramajo <grama_mauro@live.com.ar>
2020-02-17 16:48:04 -0300

    DHT11 POR SERIAL MONITOR
    Muestra la temperatura y la humedad por el serial monitor.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <DHT.h>
 
-#define LED 16
+#define DHTTYPE DHT11
+#define LEDPIN  16
+#define DHTPIN  22
+DHT dht(DHTPIN, DHTTYPE);
 
 static void task_led(void* a) {
 
 	while (1) {
 
-		digitalWrite(LED, !digitalRead(LED));
+		digitalWrite(LEDPIN, !digitalRead(LEDPIN));
 
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
 
@@ -17,11 +21,27 @@ static void task_led(void* a) {
 
 static void task_rcv(void* a) {
 
+  static char dato;
+  static float t;
+  static char temperatura[7];
+
 	while (1) {
     if(Serial.available() > 0) {
-      char dato = Serial.read();
-      if(dato == 'b') Serial.println("Sos boton");
-      else if(dato == 's') Serial.println("Sos sonso");
+      dato = Serial.read();
+      if(dato == 't') {
+        t = dht.readTemperature();
+        dtostrf(t, 6, 2, temperatura);
+        Serial.print("Temperatura: ");
+        Serial.print(temperatura);
+        Serial.println("°C");
+      }
+      else if(dato == 'h') {
+        t = dht.readHumidity();
+        dtostrf(t, 6, 2, temperatura);
+        Serial.print("Humedad: ");
+        Serial.print(temperatura);
+        Serial.println("%");
+      }
     }
 		 
 		vTaskDelay(100 / portTICK_PERIOD_MS);
@@ -32,9 +52,11 @@ static void task_rcv(void* a) {
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED, OUTPUT);
+  dht.begin();
+  pinMode(LEDPIN, OUTPUT);
   Serial.begin(9600);
-  Serial.print("Arranqué!\n");
+  Serial.println("Arranqué!");
+  Serial.println("Ingresar h para ver la humedad o t para la temperatura...");
   xTaskCreate(task_led, (const char *)"task_ledb", configMINIMAL_STACK_SIZE*2, 0, tskIDLE_PRIORITY+1, 0);
   xTaskCreate(task_rcv, (const char *)"task_rcv", configMINIMAL_STACK_SIZE*2, 0, tskIDLE_PRIORITY+1, 0);
 }
